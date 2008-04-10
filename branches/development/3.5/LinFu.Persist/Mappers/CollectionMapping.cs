1df00e727f76c6d39d989b249111be85517e80c4
philip.laureano <philip.laureano@228fa388-fe3e-0410-9462-c1833096a2b3>
2008-04-10 14:40:25 +0000

    Added the CollectionMapping class to the SVN.

---
+++ branches/development/3.5/LinFu.Persist/Mappers/CollectionMapping.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinFu.Persist.Mappers
+{
+    public class CollectionMapping : ICollectionMapping
+    {
+        public string PropertyName
+        {
+            get;
+            set;
+        }
+
+        public string SourceIDColumn
+        {
+            get;
+            set;
+        }
+
+        public string TargetIDColumn
+        {
+            get;
+            set;
+        }
+
+        public string TargetTableName
+        {
+            get;
+            set;
+        }
+
+        public Type ItemType
+        {
+            get;
+            set;
+        }
+    }
+}
