d891816f4944980343c59b0bca0da8bba4ab2ba6
philip.laureano <philip.laureano@228fa388-fe3e-0410-9462-c1833096a2b3>
2008-04-16 03:16:05 +0000

    Added ITable mapping attributes for LinFu.Persist

---
+++ branches/development/3.5/LinFu.Persist/Attributes/PersistableAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinFu.Persist
+{
+    /// <summary>
+    /// Marks the class as persistable.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface)]
+    public class PeristableAttribute : Attribute
+    {
+        /// <summary>
+        /// Creates a new instance of the <see cref="PeristableAttribute"/> class.
+        /// </summary>
+        public PeristableAttribute()
+        {
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="PeristableAttribute"/> class.
+        /// </summary>
+        /// <param name="tableName">The name of the corresponding <see cref="ITable"/></param>
+        public PeristableAttribute(string tableName)
+        {
+            TableName = tableName;
+        }
+
+        /// <summary>
+        /// Sets of gets the name of the <see cref="ITable"/>
+        /// </summary>
+        public string TableName { get; set; }
+
+    }
+}
---
+++ branches/development/3.5/LinFu.Persist/Attributes/PersistableReferenceAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinFu.Persist
+{
+    /// <summary>
+    /// Marks the propery as persistable
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class PersistableReferenceAttribute : Attribute
+    {
+        /// <summary>
+        /// Creates a new instance of the <see cref="PersistableReferenceAttribute"/> class.       
+        /// </summary>
+        public PersistableReferenceAttribute()
+        {
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="PersistableReferenceAttribute"/> class.       
+        /// </summary>
+        /// <param name="columnName">The name of the foreign key column name</param>
+        public PersistableReferenceAttribute(string columnName)
+        {
+            ColumnName = columnName;
+        }
+
+        /// <summary>
+        /// Sets or gets the name of the foreign key column name.
+        /// </summary>
+        public string ColumnName { get; set; }
+    }
+}
