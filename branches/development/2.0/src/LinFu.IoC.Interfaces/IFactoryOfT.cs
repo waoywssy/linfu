38a615d8dba606299f93e3b6ba060d5ca65f047e
Philip.Laureano <Philip.Laureano@228fa388-fe3e-0410-9462-c1833096a2b3>
2008-08-04 22:28:34 +0000

    Added XML comment documentation for the LinFu.IoC.Interfaces library

--- branches/development/2.0/src/LinFu.IoC.Interfaces/IFactoryOfT.cs
+++ branches/development/2.0/src/LinFu.IoC.Interfaces/IFactoryOfT.cs
@@ -5,8 +5,18 @@ using System.Text;
 
 namespace LinFu.IoC
 {
+    /// <summary>
+    /// A strongly-typed version of <see cref="IFactory"/>. Allows users
+    /// to create their own service instances
+    /// </summary>
+    /// <typeparam name="T">The instance type that can be created by this factory.</typeparam>
     public interface IFactory<T>
     {
+        /// <summary>
+        /// Creates a service instance using the given <paramref name="container"/>.
+        /// </summary>
+        /// <param name="container">The container that will ultimately hold the given service instance</param>
+        /// <returns>An object instance that represents the service to be created. This cannot be <c>null</c>.</returns>
         T CreateInstance(IContainer container);
     }
 }
