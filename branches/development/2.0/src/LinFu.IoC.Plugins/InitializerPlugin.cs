1b2bf7f9110aacd25c14f94275946d689d14ca4c
Philip.Laureano <Philip.Laureano@228fa388-fe3e-0410-9462-c1833096a2b3>
2008-08-15 01:57:32 +0000

    Updated the documentation for the InitalizerPlugin class.

--- branches/development/2.0/src/LinFu.IoC.Plugins/InitializerPlugin.cs
+++ branches/development/2.0/src/LinFu.IoC.Plugins/InitializerPlugin.cs
@@ -6,18 +6,32 @@ using LinFu.Reflection;
 
 namespace LinFu.IoC.Plugins
 {
+    /// <summary>
+    /// Ensures that every service instance that implments
+    /// the <see cref="IInitialize{IServiceContainer}"/> interface
+    /// will be initialized with the current service container.
+    /// </summary>
     [LoaderPlugin]
     public class InitializerPlugin : ILoaderPlugin<IServiceContainer>
     {
+        /// <summary>
+        /// Injects the initializer into the <paramref name="target"/>
+        /// container instance at the beginning of the load operation.
+        /// </summary>
+        /// <param name="target">The container being loaded.</param>
         public void BeginLoad(IServiceContainer target)
         {
             // Inject the initializer
             target.PostProcessors.Add(new Initializer());
         }
-
+        
+        /// <summary>
+        /// Overridden. This method implementation does absolutely nothing.
+        /// </summary>
+        /// <param name="target">The container being loaded.</param>
         public void EndLoad(IServiceContainer target)
         {
-            
+            // Do nothing.
         }
     }
 }
