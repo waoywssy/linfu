d3e10853127f1cf9a0208941b69149e9c1795a65
philip.laureano <philip.laureano@228fa388-fe3e-0410-9462-c1833096a2b3>
2008-09-20 14:03:46 +0000

    Added the missing PropertyInjectionFilter class to the stable branch

---
+++ branches/stable/2.0/src/LinFu.IoC.Configuration/PropertyInjectionFilter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using LinFu.IoC.Configuration.Interfaces;
+
+namespace LinFu.IoC.Configuration
+{
+    /// <summary>
+    /// A default implementation of the <see cref="IPropertyInjectionFilter"/>
+    /// class that returns properties which have the <see cref="InjectAttribute"/>
+    /// defined.
+    /// </summary>
+    [Implements(typeof(IPropertyInjectionFilter), LifecycleType.OncePerRequest)]
+    public class PropertyInjectionFilter : IPropertyInjectionFilter
+    {
+        public IEnumerable<PropertyInfo> GetInjectableProperties(Type targetType)
+        {
+            var results = from p in targetType.GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                          let attributes = p.GetCustomAttributes(typeof(InjectAttribute), false) 
+                          where attributes != null && attributes.Length > 0
+                          select p;
+
+            return results;
+        }
+    }
+}
