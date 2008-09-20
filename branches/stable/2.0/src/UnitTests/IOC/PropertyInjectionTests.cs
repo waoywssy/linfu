3acfe1812b491a77de3a2ce688fd6f7cb308dd1b
philip.laureano <philip.laureano@228fa388-fe3e-0410-9462-c1833096a2b3>
2008-09-20 15:09:44 +0000

    Changes:
    -Added the PropertyInjectionTests test fixture
    -Modified the ILMerge.MSBuild file to merge Mono.Cecil.dll into LinFu.Core.dll

---
+++ branches/stable/2.0/src/UnitTests/IOC/PropertyInjectionTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LinFu.IoC;
+using LinFu.IoC.Configuration.Interfaces;
+using NUnit.Framework;
+using SampleLibrary.IOC;
+
+namespace LinFu.UnitTests.IOC
+{
+    [TestFixture]
+    public class PropertyInjectionTests : BaseTestFixture
+    {
+        [Test]
+        public void ShouldDetermineWhichPropertiesShouldBeInjected()
+        {
+            var targetType = typeof(SampleClassWithInjectionProperties);
+            var targetProperty = targetType.GetProperty("SomeProperty");
+            Assert.IsNotNull(targetProperty);
+
+            // Load the property injection filter by default
+            var container = new ServiceContainer();
+            container.LoadFrom(AppDomain.CurrentDomain.BaseDirectory, "LinFu*.dll");
+
+            var filter = container.GetService<IPropertyInjectionFilter>();
+
+            Assert.IsNotNull(filter);
+
+            var properties = filter.GetInjectableProperties(targetType);
+            Assert.IsTrue(properties.Count() > 0);
+
+            var result = properties.First();
+            Assert.AreEqual(targetProperty, result);
+        }
+    }
+}
