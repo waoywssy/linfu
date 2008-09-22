a42afb9d23f3060144101d1b3a9f4a2a37395003
philip.laureano <philip.laureano@228fa388-fe3e-0410-9462-c1833096a2b3>
2008-09-22 06:05:57 +0000

    This is the last version of LinFu.IoC that does not have support for instantiating services with additional arguments.

---
+++ tags/2.0/2.0.2/src/LinFu.IoC.Configuration.Interfaces/IConstructorInvoke.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace LinFu.IoC.Configuration.Interfaces
+{
+    /// <summary>
+    /// Represents a type that can instantiate an object instance
+    /// using a given constructor and a given set of constructor arguments.
+    /// </summary>
+    public interface IConstructorInvoke
+    {
+        /// <summary>
+        /// Instantiates an object instance with the <paramref name="targetConstructor"/>
+        /// and <paramref name="arguments"/>.
+        /// </summary>
+        /// <param name="targetConstructor">The constructor that will be used to create the object instance.</param>
+        /// <param name="arguments">The arguments to be used with the constructor.</param>
+        /// <returns>An object reference that represents the newly-instantiated object.</returns>
+        object CreateWith(ConstructorInfo targetConstructor, object[] arguments);
+    }
+}
---
+++ tags/2.0/2.0.2/src/LinFu.IoC.Configuration/AutoPropertyInjector.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LinFu.IoC.Configuration.Interfaces;
+using LinFu.IoC.Interfaces;
+
+namespace LinFu.IoC.Configuration
+{
+    /// <summary>
+    /// A class that automatically injects property dependencies into
+    /// service instances.
+    /// </summary>
+    public class AutoPropertyInjector : IPostProcessor, IContainerPlugin
+    {
+        private static readonly HashSet<Type> _excludedServices = new HashSet<Type>(new
+            Type[] { typeof(IPropertyInjectionFilter), typeof(IArgumentResolver), typeof(IPropertySetter) });
+
+        private bool _inProcess = false;
+        /// <summary>
+        /// Automatically injects service instances
+        /// into properties as soon as they are initialized.
+        /// </summary>
+        /// <param name="result"></param>
+        public void PostProcess(IServiceRequestResult result)
+        {
+            // Prevent recursion
+            if (_inProcess)
+                return;
+            
+            lock (this)
+            {
+                _inProcess = true;
+            }
+
+            AutoInject(result);
+
+            lock (this)
+            {
+                _inProcess = false;
+            }
+        }
+
+        /// <summary>
+        /// Injects a property dependency into a target service instance.
+        /// </summary>
+        /// <param name="result">The <see cref="IServiceRequestResult"/> that will be processed for injection.</param>
+        private static void AutoInject(IServiceRequestResult result)
+        {
+            // Ignore the excluded services
+            if (_excludedServices.Contains(result.ServiceType))
+                return;
+
+            var container = result.Container;
+            var filter = container.GetService<IPropertyInjectionFilter>();
+            if (filter == null || result.ActualResult == null)
+                return;
+
+            // Determine which properties can be injected
+            var targetType = result.ActualResult.GetType();
+            var properties = filter.GetInjectableProperties(targetType).ToList();
+            if (properties.Count == 0)
+                return;
+
+            // Get the setter instance
+            var setter = container.GetService<IPropertySetter>();
+            if (setter == null)
+                return;
+
+            // Use the resolver to determine
+            // which property value should be injected
+            // into the setter
+            var resolver = container.GetService<IArgumentResolver>();
+            if (resolver == null)
+                return;
+
+            var target = result.ActualResult;
+            foreach (var property in properties)
+            {
+                var results = resolver.ResolveFrom(new Type[] { property.PropertyType }, container);
+                var propertyValue = results.FirstOrDefault();
+
+                setter.Set(target, property, propertyValue);
+            }
+        }
+
+        /// <summary>
+        /// Does absolutely nothing.
+        /// </summary>
+        /// <param name="target">The target container.</param>
+        public void BeginLoad(IServiceContainer target)
+        {
+        }
+
+        /// <summary>
+        /// Inserts the <see cref="AutoPropertyInjector"/> class at the end
+        /// of the PostProcessor chain.
+        /// </summary>
+        /// <param name="target">The target container.</param>
+        public void EndLoad(IServiceContainer target)
+        {
+            target.PostProcessors.Add(this);
+        }
+    }
+}
