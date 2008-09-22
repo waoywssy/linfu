f7d10e40e7a3705784d1faf955d352fb1b5b36de
philip.laureano <philip.laureano@228fa388-fe3e-0410-9462-c1833096a2b3>
2008-09-22 06:05:57 +0000

    This is the last version of LinFu.IoC that does not have support for instantiating services with additional arguments.

---
+++ 2.0.2/src/LinFu.IoC.Configuration/PropertyInjectionFilter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using LinFu.IoC.Configuration.Interfaces;
+using LinFu.IoC.Interfaces;
+
+namespace LinFu.IoC.Configuration
+{
+    /// <summary>
+    /// A default implementation of the <see cref="IPropertyInjectionFilter"/>
+    /// class that returns properties which have the <see cref="InjectAttribute"/>
+    /// defined.
+    /// </summary>
+    [Implements(typeof(IPropertyInjectionFilter), LifecycleType.OncePerRequest)]
+    public class PropertyInjectionFilter : IPropertyInjectionFilter, IInitialize
+    {
+        private static readonly Dictionary<Type, IEnumerable<PropertyInfo>> _propertyCache =
+            new Dictionary<Type, IEnumerable<PropertyInfo>>();
+
+        private IServiceContainer _container;
+        /// <summary>
+        /// Returns the list of <see cref="PropertyInfo"/> objects
+        /// whose setters should be injected with arbitrary values.
+        /// </summary>
+        /// <remarks>This implementation selects properties that are marked with the <see cref="InjectAttribute"/>.</remarks>
+        /// <param name="targetType">The target type that contains the target properties.</param>
+        /// <returns>A set of properties that describe which parameters should be injected.</returns>
+        public IEnumerable<PropertyInfo> GetInjectableProperties(Type targetType)
+        {
+            IEnumerable<PropertyInfo> properties = null;
+
+            // Retrieve the property list only once
+            if (!_propertyCache.ContainsKey(targetType))
+            {
+                properties = from p in targetType.GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                          let propertyType = p.PropertyType
+                          where p.CanWrite
+                          select p;
+                
+                lock(_propertyCache)
+                {
+                    _propertyCache[targetType] = properties;
+                }
+            }
+
+            properties = _propertyCache[targetType];
+
+            // The property must have the InjectAttribute defined and the
+            // service must exist in the container
+            var results = from p in properties
+                          let propertyType = p.PropertyType
+                          let isServiceArray = propertyType.ExistsAsServiceArray()
+                          let attributes = p.GetCustomAttributes(typeof(InjectAttribute), false) 
+                          where isServiceArray(_container) || _container.Contains(propertyType)
+                          && attributes != null && attributes.Length > 0
+                          select p;
+
+            return results;
+        }
+
+        /// <summary>
+        /// Initializes the <see cref="PropertyInjectionFilter"/> class.
+        /// </summary>
+        /// <param name="source">The host container.</param>
+        public void Initialize(IServiceContainer source)
+        {
+            _container = source;
+        }
+    }
+}
---
+++ 2.0.2/src/LinFu.IoC.Factories/FunctorFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LinFu.IoC.Interfaces;
+
+namespace LinFu.IoC.Factories
+{
+    /// <summary>
+    /// A class that converts a delegate into an <see cref="IFactory"/> instance.
+    /// </summary>
+    public class FunctorFactory : IFactory
+    {
+        private readonly Func<Type, IContainer, object> _factoryMethod;
+
+        /// <summary>
+        /// Initializes the class with the given <paramref name="factoryMethod"/>.
+        /// </summary>
+        /// <param name="factoryMethod">The delegate that will be used to instantiate a type.</param>
+        public FunctorFactory(Func<Type, IContainer, object> factoryMethod)
+        {
+            _factoryMethod = factoryMethod;
+        }
+
+        /// <summary>
+        /// Instantiates an object reference using the given factory method.
+        /// </summary>
+        /// <param name="serviceType">The requested service type.</param>
+        /// <param name="container">The container processing the request.</param>
+        /// <returns>A non-null object reference that represents the service type.</returns>
+        public object CreateInstance(Type serviceType, IContainer container)
+        {
+            return _factoryMethod(serviceType, container);
+        }
+    }
+}
