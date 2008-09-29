917c8fc175377394c4499f94b3775b3300528305
Philip.Laureano <Philip.Laureano@228fa388-fe3e-0410-9462-c1833096a2b3>
2008-09-29 03:48:41 +0000

    Changes:
    -Fixed some XML documentation errors in the BasePropertyInjectionFilter class.
    
    -Calling SetCustomPropertyInjectionAttribute with an attribute value of null now enables automatic property injection for all properties.
    
    -Added the ShouldAutoInjectPropertyWithoutCustomAttribute test to verify that automatic property injection works as advertised

---
+++ branches/stable/2.0/src/LinFu.IoC.Configuration/PropertyInjectionFilter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LinFu.IoC.Configuration.Interfaces;
+
+namespace LinFu.IoC.Configuration
+{
+    /// <summary>
+    /// An <see cref="IPropertyInjectionFilter"/> implementation
+    /// that automatically selects properties whose property types
+    /// currently exist in the target container.
+    /// </summary>
+    public class PropertyInjectionFilter : BasePropertyInjectionFilter
+    {
+    }
+}
