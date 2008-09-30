08005424db7d3824d2a576615d84cbe13d36818d
Philip.Laureano <Philip.Laureano@228fa388-fe3e-0410-9462-c1833096a2b3>
2008-09-30 03:42:28 +0000

    Changes:
    -Modified the method finder to match the number of method arguments against a target method when finding an alternative candidate once the best match search fails.
    
    -Added an additional set of constructors to the SampleClassWithNonServiceArgument class to make it more difficult for the MethodFinder to find the target constructor.

--- 2.0/src/SampleLibrary/IOC/SampleClassWithNonServiceArgument.cs
+++ 2.0/src/SampleLibrary/IOC/SampleClassWithNonServiceArgument.cs
@@ -12,6 +12,21 @@ namespace SampleLibrary.IOC
         {
             _value = value;
         }
+
+        // This is just a dummy constructor that
+        // will be used to confuse the constructor resolver
+        public SampleClassWithNonServiceArgument(string arg1, string arg2)
+        {
+
+        }
+
+        // This is just another dummy constructor that
+        // will be used to confuse the constructor resolver
+        public SampleClassWithNonServiceArgument(string arg1, string arg2, string arg3)
+        {
+
+        }
+
         public string Value
         {
             get { return _value; }
