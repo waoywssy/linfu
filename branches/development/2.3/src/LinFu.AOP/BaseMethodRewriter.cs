fd22470309286360c9a93c190965569f485bee7f
philip.laureano <philip.laureano@228fa388-fe3e-0410-9462-c1833096a2b3>
2009-09-07 12:04:13 +0000

    Changes (Dev Branch, v2.3):
    Added stubs for method body interception

---
+++ branches/development/2.3/src/LinFu.AOP/BaseMethodRewriter.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LinFu.AOP.Cecil.Interfaces;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+namespace LinFu.AOP.Cecil
+{
+    public abstract class BaseMethodRewriter : IMethodRewriter
+    {
+        private HashSet<TypeDefinition> _modifiedTypes = new HashSet<TypeDefinition>();
+
+        /// <summary>
+        /// Initializes a new instance of the MethodRewriter class.
+        /// </summary>
+        protected BaseMethodRewriter() { }
+
+        /// <summary>
+        /// Rewrites a target method using the given CilWorker.
+        /// </summary>
+        /// <param name="method">The target method.</param>
+        /// <param name="IL">The CilWorker that will be used to rewrite the target method.</param>
+        /// <param name="oldInstructions">The original instructions from the target method body.</param>
+        public void Rewrite(MethodDefinition method, CilWorker IL, IEnumerable<Instruction> oldInstructions)
+        {
+            var declaringType = method.DeclaringType;
+            var module = declaringType.Module;
+
+            // Interfaces and Enums cannot be modified
+            if (declaringType.IsInterface || declaringType.IsEnum)
+                return;
+
+            ImportReferences(module);
+
+            AddLocals(method);
+
+            if (!_modifiedTypes.Contains(declaringType))
+            {
+                AddAdditionalMembers(declaringType);
+                _modifiedTypes.Add(declaringType);
+            }
+
+            RewriteMethodBody(method, IL, oldInstructions);
+        }
+
+        /// <summary>
+        /// Rewrites the instructions in the target method body.
+        /// </summary>
+        /// <param name="method">The target method.</param>
+        /// <param name="IL">The <see cref="CilWorker"/> instance that represents the method body.</param>
+        /// <param name="oldInstructions">The IL instructions of the original method body.</param>
+        protected abstract void RewriteMethodBody(MethodDefinition method, CilWorker IL, IEnumerable<Instruction> oldInstructions);
+        
+
+        /// <summary>
+        /// Adds additional members to the host type.
+        /// </summary>
+        /// <param name="host">The host type.</param>
+        public virtual void AddAdditionalMembers(TypeDefinition host)
+        {
+        }
+
+        /// <summary>
+        /// Adds additional references to the target module.
+        /// </summary>
+        /// <param name="module">The host module.</param>
+        public virtual void ImportReferences(ModuleDefinition module)
+        {
+        }
+
+        /// <summary>
+        /// Adds local variables to the <paramref name="hostMethod"/>.
+        /// </summary>
+        /// <param name="hostMethod">The target method.</param>
+        public virtual void AddLocals(MethodDefinition hostMethod)
+        {
+        }       
+    }
+}
