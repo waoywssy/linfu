6ee64e0ca007c7e37fc37adf26c26a720f747ef9
philip.laureano <philip.laureano@228fa388-fe3e-0410-9462-c1833096a2b3>
2009-10-05 02:43:42 +0000

    Created the dev-2.3.1 branch from revision 429; There was an untraceable bug in LinFu.AOP v2's method body replacement that could not be fixed so I'm reverting back to the last known working version (revision 429) and redoing the method body swapper implementation.

---
+++ branches/development/2.3.1/src/LinFu.AOP.Interfaces/IAroundInvoke.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinFu.AOP.Interfaces
+{
+    /// <summary>
+    /// Represents a class that can wrap itself around any given method call.
+    /// </summary>
+    public interface IAroundInvoke : IBeforeInvoke, IAfterInvoke
+    {        
+    }
+}
---
+++ branches/development/2.3.1/src/UnitTests/AOP/MethodBodyInterceptionTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using LinFu.AOP.Cecil;
+using LinFu.AOP.Interfaces;
+using LinFu.Reflection.Emit;
+using Mono.Cecil;
+using NUnit.Framework;
+
+namespace LinFu.UnitTests.AOP
+{
+    [TestFixture]
+    public class MethodBodyInterceptionTests
+    {
+        [Test]
+        public void ShouldImplementIModifiableTypeOnModifiedSampleClass()
+        {
+            var assembly = AssemblyFactory.GetAssembly("SampleLibrary.dll");
+            var module = assembly.MainModule;
+            var typeName = "SampleClassWithNonVirtualMethod";
+
+            var targetType = (from TypeDefinition t in module.Types
+                              where t.Name == typeName
+                              select t).First();
+
+            Assert.IsNotNull(targetType);
+
+            Func<MethodDefinition, bool> methodFilter = method => method.Name == "DoSomething";
+            targetType.InterceptMethodBody(methodFilter);
+
+            var modifiedAssembly = assembly.ToAssembly();
+            var modifiedTargetType = (from t in modifiedAssembly.GetTypes()
+                                      where t.Name == typeName
+                                      select t).First();
+
+            var instance = Activator.CreateInstance(modifiedTargetType);
+            Assert.IsNotNull(instance);
+            Assert.IsTrue(instance is IModifiableType);
+        }
+    }
+}
