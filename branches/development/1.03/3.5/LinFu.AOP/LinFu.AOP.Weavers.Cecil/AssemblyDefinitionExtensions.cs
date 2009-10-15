d383c6fdec0c1cf29e1f3513eebc017c91563424
philip.laureano <philip.laureano@228fa388-fe3e-0410-9462-c1833096a2b3>
2009-10-15 22:35:24 +0000

    Branched v1.03 for maintenance purposes

---
+++ branches/development/1.03/3.5/LinFu.AOP/LinFu.AOP.Weavers.Cecil/AssemblyDefinitionExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mono.Cecil;
+
+using LinFu.AOP.CecilExtensions;
+
+namespace LinFu.AOP.Weavers.Cecil
+{
+    public static class AssemblyDefinitionExtensions
+    {
+        public static void InjectAspectFramework(this AssemblyDefinition assembly, 
+            LinFu.AOP.CecilExtensions.ITypeFilter typeFilter,
+            IMethodFilter methodFilter,
+            bool shouldInjectConstructors)
+        {
+            AspectWeaver weaver = new AspectWeaver();
+            weaver.MethodFilter = methodFilter;
+            assembly.WeaveWith(typeFilter, weaver);
+            if (!shouldInjectConstructors)
+                return;
+
+            ConstructorCrossCutter crosscutter = new ConstructorCrossCutter();
+            assembly.WeaveWith(crosscutter);
+        }
+        public static void InjectAspectFramework(this AssemblyDefinition assembly,
+            bool shouldInjectConstructors)
+        {
+            // Use default type and method filters
+            InjectAspectFramework(assembly, null, null, shouldInjectConstructors);
+        }
+        public static void InjectAspectFramework(this AssemblyDefinition assembly)
+        {
+            assembly.InjectAspectFramework(true);
+        }
+    }
+}
