ade8adf762321b0cdb35048e95ab4d8314123771
philip.laureano <philip.laureano@228fa388-fe3e-0410-9462-c1833096a2b3>
2009-12-09 21:52:34 +0000

    Refactored the MethodBodyInterceptionTests test fixture to make the code easier to read

--- 2.3.1/src/UnitTests/AOP/MethodBodyInterceptionTests.cs
+++ 2.3.1/src/UnitTests/AOP/MethodBodyInterceptionTests.cs
@@ -14,9 +14,28 @@ namespace LinFu.UnitTests.AOP
         [Test]
         public void ShouldImplementIModifiableTypeOnModifiedSampleClass()
         {
-            var assembly = AssemblyFactory.GetAssembly("SampleLibrary.dll");
-            var module = assembly.MainModule;
+            Action<object> testInstance = (instance) =>
+                           {
+                               Assert.IsNotNull(instance);
+                               Assert.IsTrue(instance is IModifiableType);
+                           };
+
+            Test(testInstance);
+        }
+
+        private void Test(Action<object> testInstance)
+        {
+            var libraryFileName = "SampleLibrary.dll";
             var typeName = "SampleClassWithNonVirtualMethod";
+            Func<MethodDefinition, bool> methodFilter = m => m.Name == "DoSomething";
+
+
+            Test(libraryFileName, typeName, methodFilter, type => Test(type, testInstance));
+        }
+        private void Test(string libraryFileName, string typeName, Func<MethodDefinition, bool> methodFilter, Action<Type> testTargetType)
+        {
+            var assembly = AssemblyFactory.GetAssembly(libraryFileName);
+            var module = assembly.MainModule;
 
             var targetType = (from TypeDefinition t in module.Types
                               where t.Name == typeName
@@ -24,17 +43,28 @@ namespace LinFu.UnitTests.AOP
 
             Assert.IsNotNull(targetType);
 
-            Func<MethodDefinition, bool> methodFilter = method => method.Name == "DoSomething";
-            targetType.InterceptMethodBody(methodFilter);
+            ModifyType(targetType, methodFilter);
+            Type modifiedTargetType = CreateModifiedType(assembly, typeName);
 
-            var modifiedAssembly = assembly.ToAssembly();
-            var modifiedTargetType = (from t in modifiedAssembly.GetTypes()
-                                      where t.Name == typeName
-                                      select t).First();
+            testTargetType(modifiedTargetType);
+        }
 
+        private void Test(Type modifiedTargetType, Action<object> testInstance)
+        {
             var instance = Activator.CreateInstance(modifiedTargetType);
-            Assert.IsNotNull(instance);
-            Assert.IsTrue(instance is IModifiableType);
+            testInstance(instance);
+        }
+
+        private Type CreateModifiedType(AssemblyDefinition assembly, string typeName)
+        {
+            var modifiedAssembly = assembly.ToAssembly();
+            return (from t in modifiedAssembly.GetTypes()
+                    where t.Name == typeName
+                    select t).First();
+        }
+        private void ModifyType(TypeDefinition targetType, Func<MethodDefinition, bool> methodFilter)
+        {
+            targetType.InterceptMethodBody(methodFilter);
         }
     }
 }
